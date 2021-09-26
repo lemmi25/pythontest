f9630613d97cf7554b6029d2a208722e4479a8d1
Moritz Bösenberg <moritz.boesenberg@outlook.com>
2021-09-26 17:13:16 +0200

    fixed python package issues

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='python_test',
+      version='0.0.1',
+      description='Shows how to use setup.py',
+      author='Moritz Bösenberg',
+      license='MIT',
+      packages=['python_test'],
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='tutorial',
+      include_package_data = True,
+)
